d7afbbf571314f812a675f5de1c32f44d60a13e3
Anna Smith <annaleighsmith60@gmail.com>
2024-04-23 15:28:54 -0400

    switched to kickstart

--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
-local opt = vim.opt
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.cursorline = true -- Enable highlighting of the current line
